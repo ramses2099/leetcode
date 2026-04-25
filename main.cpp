f36db457042edab20b54954473c9940aed33020e
ramses2099 <ramses2099@gmail.com>
2026-04-25 12:50:28 -0400

    first commit

--- main.cpp
+++ main.cpp
@@ -3,15 +3,36 @@
 
 using namespace std;
 
+class Solution
+{
+public:
+    int strStr(string haystack, string needle)
+    {
+        return 0;
+    }
+};
+
 int main()
 {
+    // defualt setting
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    // use debug template
+    // char Char = 'A';
+    // int arr[] = {1, 2, 3, 4};
+    // bitset<8> Bitset(100);
+    // map<string, int> map_String_Int = {{"apple", 5}, {"banana", 3}, {"orange", 7}};
 
-    char Char = 'A';
-    int arr[] = {1, 2, 3, 4};
-    bitset<8> Bitset(100);
-    map<string, int> map_String_Int = {{"apple", 5}, {"banana", 3}, {"orange", 7}};
+    // debug(Char, arr, Bitset, map_String_Int);
 
-    debug(Char, arr, Bitset, map_String_Int);
+    string haystack = "";
+    string needle = "";
+    int output = 0;
+
+    class Solution stion;
+    int rs = stion.strStr(haystack, needle);
+    assert((rs == output));
 
     return EXIT_SUCCESS;
 }
